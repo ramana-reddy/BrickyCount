fcffde2e29543164eb95a07b61bf6563a876f963
Ramana Reddy <rr@gametech.com>
2016-05-14 09:08:52 +0100

    Initial Commit

---
+++ RummyCount/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RummyCount
+//
+//  Created by Venkata Mandala on 14/05/2016.
+//  Copyright © 2016 Ramana Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
